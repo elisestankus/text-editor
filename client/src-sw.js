991f76e73d97a0d8a20e0be1abbadb229c4671eb
estan <elise.stankus@gmail.com>
2024-02-19 16:21:48 -0800

    defined files to be cached in registerRoute function

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,13 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+// register route for caching assets (css and js)
+registerRoute(
+  ({ request }) => {
+    // defines the file types being cached
+    return (
+      request.destination === 'style' ||
+      request.destination === 'script'
+    );
+  },
+);
