f477ad74e63b06cd72b583bd3ad0ef62f7e5b809
estan <elise.stankus@gmail.com>
2024-02-19 17:21:47 -0800

    added putDb method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,8 +13,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Put to db');
+  // creates connection to db, version 1
+  const jateDb = await openDB('jate', 1);
+  // create transaction, define db and privileges
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // open object store
+  const store = tx.objectStore('jate');
+  const request = store.put({ value: content })
+  const result = await request;
 
+}
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
